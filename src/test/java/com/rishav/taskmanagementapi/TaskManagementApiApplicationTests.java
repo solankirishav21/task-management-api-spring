861b9b8d78714a5ef25254ba6bc482734d9729de
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2026-05-04 00:01:46 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/rishav/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishav.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
