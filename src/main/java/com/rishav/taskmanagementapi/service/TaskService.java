bb290d9339686d46007f738f60e19aa4e70d8f10
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2026-05-04 19:27:17 +0530

    Service logic to add and get all tasks

---
+++ src/main/java/com/rishav/taskmanagementapi/service/TaskService.java
@@ -0,0 +1,27 @@
+package com.rishav.taskmanagementapi.service;
+
+
+import com.rishav.taskmanagementapi.model.Task;
+import com.rishav.taskmanagementapi.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+}
