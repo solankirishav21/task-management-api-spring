95c52ecc24c92d699886e7502a5747b4bd6c0d93
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2026-05-04 19:38:40 +0530

    controller for post and get task

---
+++ src/main/java/com/rishav/taskmanagementapi/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.rishav.taskmanagementapi.controller;
+
+
+import com.rishav.taskmanagementapi.model.Task;
+import com.rishav.taskmanagementapi.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public String homeController(){
+        return "Home Page";
+    }
+
+    @PostMapping("/task")
+    public Task createTask(@RequestBody Task task){
+        return taskService.createTask(task);
+    }
+
+    @GetMapping("/task")
+    public List<Task> getAllTask(){
+        return taskService.getAllTasks();
+    }
+}
